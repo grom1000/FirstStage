4850cf02819bac8300c047b733dc8d68ed32a8b5
Nikolay <ntamarin27@gmail.com>
2020-01-23 19:22:09 +0300

    fourth commit

--- src/script/burger.js
+++ src/script/burger.js
@@ -1,10 +1,24 @@
-document.querySelector('.hamburger').onclick = menuShow;
-document.querySelector('.menu').onclick = menuHide;
+var menu = document.querySelector('.menu');
+var hamburger = document.querySelector('.hamburger');
+var close = document.querySelector('.close');
+var promo = document.querySelector('.promo')
+var backdrop = document.querySelector('.backdrop');
+var overlay = document.querySelector('.overlay');
+var list = document.querySelector('.menu__list');
 
 function menuShow() {
-    document.querySelector('.menu').style.left = 0;
+    menu.style.left = 0;
+    backdrop.style.left = 0;
+    backdrop.style.opacity = 1;
 }
 
 function menuHide() {
-    document.querySelector('.menu').style.left = '-12.5rem';
+    menu.style.left = '-12.5rem';
+    backdrop.style.left = '-100%';
+    backdrop.style.opacity = 0;
 }
+
+hamburger.onclick = menuShow;
+close.onclick = menuHide;
+backdrop.onclick = menuHide;
+list.onclick = menuHide;
