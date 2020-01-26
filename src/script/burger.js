755721b77eb1be31ce609b19255b3a281fd6c22a
Nikolay <ntamarin27@gmail.com>
2020-01-26 19:53:45 +0300

    fourth commit fix

--- src/script/burger.js
+++ src/script/burger.js
@@ -1,24 +1,39 @@
-var menu = document.querySelector('.menu');
-var hamburger = document.querySelector('.hamburger');
-var close = document.querySelector('.close');
-var promo = document.querySelector('.promo')
-var backdrop = document.querySelector('.backdrop');
-var overlay = document.querySelector('.overlay');
-var list = document.querySelector('.menu__list');
+window.onload = function() {
 
-function menuShow() {
-    menu.style.left = 0;
-    backdrop.style.left = 0;
-    backdrop.style.opacity = 1;
+    var menu = document.querySelector('.menu');
+    var hamburger = document.querySelector('.hamburger');
+    var close = document.querySelector('.close');
+    var backdrop = document.querySelector('.backdrop');
+    var list = document.querySelector('.menu__list');
+    var width = 0;
+    var menuOpen = false;
+
+    function browserSize() {
+        width = window.innerWidth;
+        if (width >= 769) {
+            menuHide()
+        } else if (width < 769 && menuOpen == true) {
+            menuShow()
+        }
+    }
+
+    function menuShow() {
+        menuOpen = true
+        menu.style.left = 0;
+        backdrop.style.left = 0;
+        backdrop.style.opacity = 1;
+        }
+
+    function menuHide() {
+        menuOpen = false;
+        menu.style.left = '-12.5rem';
+        backdrop.style.left = '-100%';
+        backdrop.style.opacity = 0;
+    }
+
+    hamburger.onclick = menuShow;
+    close.onclick = menuHide;
+    backdrop.onclick = menuHide;
+    list.onclick = menuHide;
+    window.onresize = browserSize;
 }
-
-function menuHide() {
-    menu.style.left = '-12.5rem';
-    backdrop.style.left = '-100%';
-    backdrop.style.opacity = 0;
-}
-
-hamburger.onclick = menuShow;
-close.onclick = menuHide;
-backdrop.onclick = menuHide;
-list.onclick = menuHide;
